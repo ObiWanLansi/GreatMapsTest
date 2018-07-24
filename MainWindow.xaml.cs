4a8bb0237be7b8f4d9b4889239de27df5b225614
ObiWanLansi <joerg.lanser@gmail.com>
2018-07-24 08:33:46 +0200

    Testing With GMAP

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+
+
 
 namespace GreatMapsTest
 {
@@ -20,9 +16,67 @@ namespace GreatMapsTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            GMapProvider.WebProxy = WebRequest.DefaultWebProxy;
+            GMapProvider.WebProxy.Credentials = CredentialCache.DefaultCredentials;
+
+            mcMapControl.DragButton = MouseButton.Left;
+
+            mcMapControl.MapProvider = GMapProviders.OpenStreetMap;
+
+            mcMapControl.Manager.Mode = AccessMode.ServerAndCache;
+
+            mcMapControl.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            mcMapControl.ShowCenter = false;
+            mcMapControl.MinZoom = 2;
+            mcMapControl.MaxZoom = 22;
+
+            mcMapControl.Position = new PointLatLng( 49.761471 , 6.650053 );
+            mcMapControl.Zoom = 14;
         }
-    }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_CreatePolygon_Click( object sender , RoutedEventArgs e )
+        {
+            GMapPolygon polygon = new GMapPolygon( new List<PointLatLng>
+            {
+                new PointLatLng(49,6),
+                new PointLatLng(50,6),
+                new PointLatLng(50,7),
+                new PointLatLng(49,7)
+            } );
+
+            mcMapControl.Markers.Add( polygon );
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_CreateRoute_Click( object sender , RoutedEventArgs e )
+        {
+            GMapRoute route = new GMapRoute( new List<PointLatLng>
+            {
+                new PointLatLng(49.1,6),
+                new PointLatLng(49.5,6.1),
+                new PointLatLng(50,6),
+                new PointLatLng(47,7)
+            } );
+
+            mcMapControl.Markers.Add( route );
+        }
+    } // end public partial class MainWindow 
 }
